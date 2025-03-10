ae0b93485795afc0d5b47fa8f52f330028c20fb2
Shazaib Amir <shazaibgit@gmail.com>
2025-03-10 15:04:48 +0000

    completed 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let finalString = "";
 
-};
+    if (num<0) {
+        return "ERROR";
+    }
+    else {
+    for (i=0;i<num;i++) {
+        finalString = finalString + str;
+
+    }
+    return finalString;
+
+}
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let lengthOfString = str.length;
+    let emptyString = "";
+
+    for (i=0;i<lengthOfString;i++){
+         emptyString += str[str.length-1];
+         str = str.slice(0,-1);
+
+    }
+    return emptyString;
 
 };
-
+console.log(reverseString('hello'))
 // Do not edit below this line
 module.exports = reverseString;
