8933028c2007b63548ed514666d04048b988f172
Shazaib Amir <shazaibgit@gmail.com>
2025-03-18 17:31:01 +0000

    completed ex 4-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arrays) {
+    for (i=1 ; i<arrays[0].length;i++ ){
+        if (  arrays[0][i] === arrays[i]){
+            arrays[0].splice(i,1);
+            i--
+
+        }
+        else {
+            continue;
+        }
+    }
+    return arrays[0];
+
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let max = Math.max(a,b);
+    let min = Math.min(a,b);
+    let sum = 0;
 
+    if (a<0 || b <0 || Number.isInteger(a) == false || Number.isInteger(b) == false || isNaN(a) || isNaN(b)){
+        return 'ERROR';
+    }
+    else {
+    for (let i = min; i<=max; i++) {
+    
+        sum = sum+i;
+    }
+    return sum;
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+ 
